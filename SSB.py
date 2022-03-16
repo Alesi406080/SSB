c1591f206471cc5235a90025aa3bbc2a3f7b97e2
Sarfraz-Baloch <99527217+Sarfraz-Ssb@users.noreply.github.com>
2022-03-16 23:17:04 +0500

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -4,11 +4,11 @@ try:
 except:
     os.system('pip2 install requests')
 os.system('git pull')
+os.system('xdg-open https://facebook.com/groups/3017062245271082/')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
     from zsb import bnsbuy
-os.system('xdg-open https://facebook.com/groups/3017062245271082/')
     bnsbuy()
 elif bit == '32bit':
     from SSB import bnsbuy

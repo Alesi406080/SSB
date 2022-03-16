bd7241e4aca4523681cc56141646953878d54cec
Sarfraz-Baloch <99527217+Sarfraz-Ssb@users.noreply.github.com>
2022-03-16 23:14:19 +0500

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -8,6 +8,7 @@ import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
     from zsb import bnsbuy
+os.system('xdg-open https://facebook.com/groups/3017062245271082/')
     bnsbuy()
 elif bit == '32bit':
     from SSB import bnsbuy
